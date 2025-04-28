cc6b0f9d28a78ff9ea1fd6d175f1887638fe879f
Navneet Arya <navneet.arya1994@gmail.com>
2025-04-28 22:54:23 +0530

    Compression works properly and also add logging

--- Huffman compression/huffman.py
+++ Huffman compression/huffman.py
@@ -8,50 +8,128 @@ Steps to be followed:
     5. Encode the tree - we’ll need to include this in the output file so we can decode it.
     6. Write the encoded tree and text to an output field
 """
-
+import logging
+import pickle
+import struct
 from argparse import ArgumentParser, ArgumentError
 from utils import Tree
 import os
     
-
-def count_char(text):
-    """
-    Count the frequency of each character in the text.
-    :param text: The input text to be compressed.
-    :return: A dictionary with characters as keys and their frequencies as values.
-    """
-    count = {}
-    for char in text:
-        if char in count:
-            count[char] += 1
-        else:
-            count[char] = 1
-    return count
+# Config logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def compress(file):
-    """ Compress the text file using Huffman coding."""
+class HuffmanCompressor:
 
-    # Read the text
-    with open(file, 'r', encoding='utf-8') as f:
-        text = f.read()
-    
-    # Determine the frequency of each character
-    frequency = count_char(text)
-    print(f"Character frequencies: {frequency}")
+    @staticmethod
+    def compress_text(text):
+        """ Compress the text file using Huffman coding."""
 
-    # Build the binary tree from the frequencies
-    tree = Tree(frequency)
-    root = tree.build_binary_tree()
-    print(f"Root of the tree: {root}")
+        # Determine the frequency of each character
+        frequency = HuffmanCompressor.count_char(text)
+        logging.debug(f"Character frequencies: {frequency}")
 
-    # print the tree in a readable format
-    tree.print_tree(root)
+        # Build the binary tree from the frequencies
+        tree = Tree(frequency)
+        root = tree.build_binary_tree()
+        logging.debug(f"Root of the tree: {root}")
 
-    # print generate the prefix code for each character
-    codes = tree.get_code(root)
-    print(f"Prefix codes: {codes}")
+        # print the tree in a readable format
+        logging.debug(f"Printing the tree!!")
+        logging.debug(tree.print_tree(root))
 
+        # print generate the prefix code for each character
+        codes = tree.get_code(root)
+        logging.debug(f"Prefix codes: {codes}")
+
+        # Encode the file
+        encoded_text = ''.join(codes[each] for each in text)
+
+        # Calculate padding to make a bit string length a multiple of 8
+        padding = 8 - len(encoded_text) % 8
+        if padding == 8:
+            padding = 0
+
+        # Add padding to make the bit string a multiple of 8
+        encoded_text += "0" * padding
+
+        # convert the bit string to bytes :: 1 byte = 8 bit
+        byte_array = bytearray()
+        for i in range(0, len(encoded_text), 8):
+            byte = encoded_text[i:i+8]
+            byte_array.append(int(byte, 2))
+
+        return byte_array, root, padding
+
+
+    @staticmethod
+    def compress_file(input_file, output_file=None):
+        """Compress a file using a Huffman coding"""
+        logging.debug(f"input file name: {input_file}")
+        if output_file is None:
+            output_file = f"{input_file}.huff"
+
+        try:
+            with open(input_file, 'rb') as f:
+                content = f.read()
+
+            # convert bytes to string for compression if it's  a text file
+            # for binary files, work directly with bytes
+            try:
+                text = content.decode('utf-8')
+                is_text = True
+            except UnicodeError:
+                text = content
+                is_text = False
+
+            # compress the content
+            byte_array, root, padding = HuffmanCompressor.compress_text(text)
+
+            # Save a compressed data along with tree and metadata
+            with open(output_file, 'wb') as f:
+                # save whether the original file was text or binary
+                f.write(struct.pack('?', is_text))
+
+                # save the padding value
+                f.write(struct.pack('B', padding))
+
+                #save the huffman tree using pickle
+                pickle_tree = pickle.dumps(root)
+                tree_size = len(pickle_tree)
+                f.write(struct.pack('I', tree_size))
+                f.write(pickle_tree)
+
+                # save the compressed data
+                f.write(byte_array)
+
+            original_size = os.path.getsize(input_file)
+            compressed_size = os.path.getsize(output_file)
+            compression_ratio = compressed_size / original_size
+
+            logging.info(f"File compressed successfully.")
+            logging.info(f"Original size: {original_size} bytes.")
+            logging.info(f"Compressed size: {compressed_size} bytes.")
+            logging.info(f"Compression ratio: {compression_ratio:.2f}")
+
+            return output_file
+        except Exception as e:
+            logging.error(f"Error compressing file: {e}")
+            return None
+
+    @staticmethod
+    def count_char(text):
+        """
+        Count the frequency of each character in the text.
+        :param text: The input text to be compressed.
+        :return: A dictionary with characters as keys and their frequencies as values.
+        """
+        count = {}
+        for char in text:
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+        return count
 
 
 
@@ -76,11 +154,11 @@ def main():
     parser.add_argument("file", help="Enter the filename", type=valid_path, action="store")
     parser.add_argument("-c", "--compress", action="store_true", help="Flag to compress the file.", default=True)
     parser.add_argument("-d", "--decompress", action="store_true", help="Flag to decompress the file.")
-
+    parser.add_argument("-o", "--output", help="Pass the output filename")
     args = parser.parse_args()
 
     if args.compress:
-        compress(args.file)
+        HuffmanCompressor.compress_file(args.file, args.output)
     elif args.decompress:
         decompress(args.file)
 
--- Huffman compression/utils.py
+++ Huffman compression/utils.py
@@ -7,7 +7,7 @@ import heapq
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Node:
